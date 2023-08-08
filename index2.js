84f37efbb32a4f1b0a3754ba8023cea9c06c403d
jvarghe <vcs.inbox@outlook.com>
2023-08-08 00:23:09 +0000

    Add 'index2.js' and 'iss_promised.js'. Add functionality so its working like 'index.js' and 'iss.js'.

--- index2.js
+++ index2.js
@@ -6,6 +6,15 @@
  * `iss.js`. The version WITH promises is implemented in `index2.js` and
  * `iss_promised.js`.
  *
+ * This file also has two ways of invoking it: Primary & Secondary. The primary
+ * invocation just calls the `nextISSTimesForMyLocation()` method from
+ * `iss_promised.js`. This method handles invoking all the asynchronous
+ * functions, processes the data and returns only the rise times. The secondary
+ * invocation is just a test—checking how to use the return values from
+ * promises. It directly calls all three promises in `iss_promised.js` one at
+ * a time.
+ *
+ *
  * THE CHALLENGE: ISS SPOTTER WITH PROMISES
  *
  * In order to use promises, our HTTP request library needs to support them.
@@ -19,3 +28,105 @@
  * popular. We pick it because it is related to the `request` library, which
  * we've already used and are somewhat comfortable with.
  */
+
+
+// IMPORTS
+
+// FOR SECONDARY INVOCATION
+// const { fetchMyIP } = require("./iss_promised.js");
+// const { fetchCoordsByIP } = require("./iss_promised.js");
+// const { fetchISSFlyOverTimes } = require("./iss_promised.js");
+
+// FOR PRIMARY INVOCATION
+const { nextISSTimesForMyLocation } = require("./iss_promised.js");
+
+
+
+// This function will simply print fly-over times and durations of the ISS in
+// an easy readable format.
+//
+// Note: For some reason, importing this file from `index.js` results in
+// errors, so `printFlyoverTimes()` was copied here.
+const printFlyoverTimes = function(flyoverTimes) {
+
+  // Iterate over the object...
+  for (const passOverTime of flyoverTimes) {
+
+    // Create a new `datetime` object.
+    const datetime = new Date(0);
+
+    // Add the `risetime` to it.
+    datetime.setUTCSeconds(passOverTime.risetime);
+
+    // Add the duration to the datetime.
+    const duration = passOverTime.duration;
+
+    // Log it to console.
+    console.log(`The ISS will next pass over your location at ${datetime} for ${duration} seconds!`);
+  }
+
+};
+
+
+
+
+// DRIVER CODE
+
+// PRIMARY INVOCATION
+// Calls `nextISSTimesForMyLocation` from `index2.js` and executes it.
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+
+    console.log(`These are the flyoverTimes for your location: \n`);
+    printFlyoverTimes(passTimes);
+
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
+
+
+
+// SECONDARY INVOCATION: CALLING PROMISES ONE-BY-ONE AND DOING THINGS WITH THEM
+
+// This is an alternate to the function call to `nextISSTimesForMyLocation()`.
+// It calls the promises one-by-one and does things with the data returned from
+// each promise. Clearly promises also need to be staggered in a waterfall
+// sort of way, just like callbacks.
+
+// fetchMyIP()
+
+//   .then((responseBody) => {
+
+//     console.log(`This is the RAW response body of the IP Request:
+//       \n ${responseBody}.\n`);
+
+//     fetchCoordsByIP(responseBody)
+
+//       .then((geoCoords) => {
+
+//         console.log(`This the RAW response body of your Geo-Coordinates Request:
+//           \n ${geoCoords}.\n`);
+//         fetchISSFlyOverTimes(geoCoords)
+
+//           .then((riseTimesJSON) => {
+
+//             console.log(`These are the RAW rise times at your location:
+//               \n ${riseTimesJSON}.\n`);
+
+//             // Parse the JSON string's `response` key section into an object.
+//             const flyoverTimes = JSON.parse(riseTimesJSON).response;
+
+//             console.log(`These are the flyoverTimes for your location: \n`);
+
+//             printFlyoverTimes(flyoverTimes);
+
+//           });
+
+//       });
+
+//   })
+
+//   .catch((error) => {
+//     console.log("It didn't work: ", error.message);
+//   });
--- iss_promised.js
+++ iss_promised.js
@@ -1,2 +1,79 @@
 // IMPORTS
 const requestPN = require("request-promise-native");
+
+
+/*
+ * Requests user's ip address from https://www.ipify.org/
+ *
+ * Input: None
+ * Returns: Promise of request for ip data, returned as JSON string.
+ */
+const fetchMyIP = function() {
+
+  return requestPN(`https://geo.ipify.org/api/v2/country?apiKey=at_IJJYLKOpKd3XL8cLMLHrV37F4GSGr`);
+
+};
+
+
+/*
+ * Makes a request to `ipwho.is` using the provided IP address to get its
+ * geographical information (latitude/longitude).
+ *
+ * Input: JSON string containing the IP address.
+ * Returns: Promise of request for latitude & longitude.
+ */
+const fetchCoordsByIP = function(body) {
+
+  // The body looks like a JSON string. It will need to deserialized into
+  // a JavaScript Object.
+  const bodyObj = JSON.parse(body);
+
+  // Retrieve the IP Address field from `bodyObj`.
+  const ipAddress = bodyObj.ip;
+
+  // We will use the `IPWho` service. This is its co-ordinates.
+  return requestPN(`http://ipwho.is/${ipAddress}`);
+
+};
+
+
+/*
+ * Requests data from https://iss-flyover.herokuapp.com using provided latitude
+ * & longitude data.
+ *
+ * Input: JSON body containing geo data response from `ipwho.is`.
+ * Returns: Promise of request for fly over data, returned as JSON string.
+ */
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return requestPN(url);
+};
+
+
+/*
+ * Input: None
+ *
+ * Returns: Promise for fly over data for users location.
+ */
+const nextISSTimesForMyLocation = function() {
+
+  // Call the three other functions in sequence.
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+
+};
+
+
+// EXPORTS
+
+// FOR PRIMARY INVOCATION
+module.exports = { nextISSTimesForMyLocation };
+
+// FOR SECONDARY INVOCATION
+// module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
